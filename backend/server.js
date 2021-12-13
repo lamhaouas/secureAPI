8dfa556ddea3ac12222a2b96d4067bec4b5b6100
Si mohamed <m.lamhaouas@gmail.com>
2021-12-13 14:26:37 -0500

    procfile/update port for heroku

--- backend/server.js
+++ backend/server.js
@@ -43,5 +43,10 @@ server.on('listening', () => {
     const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
     console.log('Listening on ' + bind);
 });
+// heroku port
+
+if (port == null || port == "") {
+    port = 3000;
+}
 
 server.listen(port);
